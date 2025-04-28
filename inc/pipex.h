dd2caf38e9952ee2f65b77074113c651607eab48
4n4k1n <anakin@pregitzer.de>
2025-04-28 23:52:12 +0200

    added new functions

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 10:35:53 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/28 16:52:08 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/28 23:16:19 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,9 @@
 # include <fcntl.h>
 # include "../libft/inc/libft.h"
 
-// parsing
 int		parsing(int ac, char **av);
-
-// error function
 void	error(char *msg);
-
-// process the parent/cchilld process
 int	    process(char *av, char **ev);
-
-// get the right path
-char    *get_path(char *command, char *ev);
+char *get_path(char *command, char **ev);
 
 #endif

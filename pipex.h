156efb5e5cbb406a1574b3b04ad1ac3ebb1f4c6b
4n4k1n <anakin@pregitzer.de>
2025-04-28 14:00:05 +0200

    created headerfile

---
+++ pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/26 10:35:53 by apregitz          #+#    #+#             */
+/*   Updated: 2025/04/27 23:14:54 by apregitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <string.h>
+# include <fcntl.h>
+
+// parsing
+int	parsing(int ac, char **av);
+void	error(char *msg);
+
+
+#endif

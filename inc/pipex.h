e8eaf58140e2df9ef0d726300a3a93962164be07
4n4k1n <anakin@pregitzer.de>
2025-04-28 16:53:22 +0200

    removed the unused libft fheader and added new functions to the pipex header

--- inc/pipex.h
+++ inc/pipex.h
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 10:35:53 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/28 14:56:40 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/28 16:52:08 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,18 @@
 # include <unistd.h>
 # include <string.h>
 # include <fcntl.h>
-# include "libft.h"
+# include "../libft/inc/libft.h"
 
 // parsing
-
 int		parsing(int ac, char **av);
+
+// error function
 void	error(char *msg);
+
+// process the parent/cchilld process
+int	    process(char *av, char **ev);
+
+// get the right path
+char    *get_path(char *command, char *ev);
 
 #endif

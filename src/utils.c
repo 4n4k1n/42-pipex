92792e151e29ad647fbebe90c0d365283b7d0cc5
4n4k1n <anakin@pregitzer.de>
2025-04-28 16:54:23 +0200

    added function to process the child/parent process and to fund the right executeble path

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/27 23:13:12 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/28 15:01:01 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/28 16:50:12 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,20 @@ void	error(char *msg)
 	exit(1);
 }
 
+char *get_path(char *command, char *ev);
+
 int	process(char *av, char **ev)
 {
 	char	**command;
 	char	*path;
 	int		i;
 
+	while (ev[i] && !ft_strnstr(ev[i], "PATH", 4))
+		i++;
+	if (!ev)
+		return (0);
+	command = ft_split(av[i], ' ');
+	if (!command)
+		return (0);
 	return (1);
 }

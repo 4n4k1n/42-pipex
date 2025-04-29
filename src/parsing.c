deadb0a0c2a4767447dbe54c8a340a3917e328b8
4n4k1n <anakin@pregitzer.de>
2025-04-29 16:41:31 +0200

    fixed the return value

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 14:07:49 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/28 14:56:57 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/04/29 00:28:00 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ int	parsing(int ac, char **av)
 		return (1);
 	write(1, "Invalid arguments!\n", 19);
 	write(1, "./pipex <file1> <cmd1> <cmd2> <file2>\n", 38);
-	return (1);
+	return (0);
 }

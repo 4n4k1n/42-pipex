bc77a2c0f2e064cc5b1a68171c46c713e2aab76b
4n4k1n <anakin@pregitzer.de>
2025-05-06 11:23:18 +0200

    using struct now to store all the data. created function to init the struct and one for preparing the child execution

--- src/pipex.c
+++ src/pipex.c
@@ -6,79 +6,59 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 10:35:34 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/01 10:57:08 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/06 11:08:10 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
-// function to process the child
-
-void	child(char **av, char **ev, int *fds, pid_t pid)
+void	build_child(t_data *data, int last)
 {
-	int	input;
+	int	fd[2];
 
-	input = open(av[1], O_RDONLY, 0777);
-	if (input == -1)
+	if (!last && pipe(fd) == -1)
 		error();
-	dup2(input, STDIN_FILENO);
-	dup2(fds[1], STDOUT_FILENO);
-	close(fds[0]);
-	close(fds[1]);
-	close(input);
-	process(av[2], ev, pid);
+	if (last)
+		data->fd[1] = data->output_file;
+	else
+		data->fd[1] = fd[1];
+	exec_cmd(data);
+	if (last)
+		close(data->fd[1]);
+	else
+	{
+		close(fd[1]);
+		close(data->fd[0]);
+		data->fd[0] = fd[0];
+	}
 }
 
-// function to process the parent
-
-void	parent(char **av, char **ev, int *fds, pid_t pid)
+int	init_struct(t_data *data, int ac, char **av, char **ep)
 {
-	int	output;
-
-	output = open(av[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
-	if (output == -1)
-		error();
-	dup2(fds[0], STDIN_FILENO);
-	dup2(output, STDOUT_FILENO);
-	close(fds[0]);
-	close(fds[1]);
-	close(output);
-	process(av[3], ev, pid);
+	data->input_file = open(av[1], O_RDONLY, 0777);
+	if (data->input_file == -1)
+		return (0);
+	data->output_file = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (data->output_file == -1)
+		return (close(data->input_file), 0);
+	data->fd[0] = data->input_file;
+	data->ac = ac;
+	data->ep = ep;
+	data->av = av;
+	data->i = 1;
+	return (1);
 }
 
-void	open_pids(pid_t *pid1, pid_t *pid2)
+int	main(int ac, char **av, char **ep)
 {
-	*pid1 = fork();
-	if (*pid1 != 0)
-		*pid2 = fork();
-}
+	t_data	data;
 
-void	close_pids(pid_t *pid1, pid_t *pid2)
-{
-	int	status;
-
-	waitpid(*pid1, NULL, 0);
-	waitpid(*pid2, &status, 0);
-	if (WIFEXITED(status))
-		exit(WEXITSTATUS(status));
-}
-
-int	main(int ac, char **av, char **ev)
-{
-	pid_t	pid1;
-	pid_t	pid2;
-	int		fds[2];
-
-	(void)ac;
 	if (!parsing(ac, av))
 		return (1);
-	if (pipe(fds) == -1)
-		perror("pipe");
-	open_pids(&pid1, &pid2);
-	if (pid1 == 0)
-		child(av, ev, fds, pid1);
-	if (pid2 == 0)
-		parent(av, ev, fds, pid2);
-	close_pids(&pid1, &pid2);
+	if (!init_struct(&data, ac, av, ep))
+		return (1);
+	while (++(data.i) < ac - 2)
+		build_child(&data, 0);
+	build_child(&data, 1);
 	return (0);
 }

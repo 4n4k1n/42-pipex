db4b1c699b5a8af2ed58d90dd6daeff66ee19963
4n4k1n <anakin@pregitzer.de>
2025-05-10 00:11:41 +0200

    splitted the init_struct function into 2 functions

--- src/pipex.c
+++ src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 10:35:34 by apregitz          #+#    #+#             */
-/*   Updated: 2025/05/09 22:00:19 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/10 00:10:48 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	build_child(t_data *data, int last)
 
 	if (!last && pipe(fd) == -1)
 		error();
+	// printf("fd[0]=%d\nfd[1]=%d\n", fd[0], fd[1]);
 	if (last)
 		data->fd[1] = data->output_file;
 	else
@@ -33,30 +34,42 @@ void	build_child(t_data *data, int last)
 	}
 }
 
-int	init_struct(t_data *data, int ac, char **av, char **ep)
+int	open_files(t_data *data)
 {
 	int	hd_flags;
 	int	flags;
 
 	hd_flags = O_WRONLY | O_CREAT | O_APPEND;
 	flags = O_WRONLY | O_CREAT | O_TRUNC;
-	if (data->input_file == -1)
-		return (0);
-	if (BONUS && ft_strncmp(av[1], "here_doc", 9) == 0)
-		data->output_file = open(av[ac - 1], hd_flags, 0777);
+	if (BONUS && ft_strncmp(data->av[1], "here_doc", 9) == 0)
+	{
+		data->output_file = open(data->av[data->ac - 1], hd_flags, 0777);
+		if (data->output_file == -1)
+			error();
+	}
 	else
 	{
-		data->input_file = open(av[1], O_RDONLY, 0777);
+		data->input_file = open(data->av[1], O_RDONLY, 0777);
+		if (data->input_file == -1)
+			error();
 		data->fd[0] = data->input_file;
-		data->output_file = open(av[ac - 1], flags, 0777);
+		data->output_file = open(data->av[data->ac - 1], flags, 0777);
 		if (data->output_file == -1)
 			return (close(data->input_file), 0);
 	}
+	data->fd[0] = data->input_file;
+	return (1);
+}
+
+int	init_struct(t_data *data, int ac, char **av, char **ep)
+{
+	
 	data->fd[0] = data->input_file;
 	data->ac = ac;
 	data->ep = ep;
 	data->av = av;
 	data->i = 1;
+	open_files(data);
 	return (1);
 }
 

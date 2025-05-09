95b4a53e9653a210478f668380daa4c6abef5e79
4n4k1n <anakin@pregitzer.de>
2025-05-09 20:44:28 +0200

    added the parsing for the bonus

--- src/parsing.c
+++ src/parsing.c
@@ -6,17 +6,27 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 14:07:49 by apregitz          #+#    #+#             */
-/*   Updated: 2025/04/29 00:28:00 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/09 20:36:48 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
-int	parsing(int ac, char **av)
+int	parsing(int ac)
 {
-	if (ac == 5 && av)
-		return (1);
-	write(1, "Invalid arguments!\n", 19);
-	write(1, "./pipex <file1> <cmd1> <cmd2> <file2>\n", 38);
+	if (BONUS)
+	{
+		if (ac >= 5)
+			return (1);
+		write(1, "Invalid arguments!\n", 19);
+		write(1, "./pipex <file1> <cmd1> <cmd2> ... <file2>\n", 42);
+	}
+	else
+	{
+		if (ac == 5)
+			return (1);
+		write(1, "Invalid arguments!\n", 19);
+		write(1, "./pipex <file1> <cmd1> <cmd2> <file2>\n", 38);
+	}
 	return (0);
 }

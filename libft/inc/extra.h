b13cad8b068bc9d9a3a417c0eb3034fad3e834f5
4n4k1n <anakin@pregitzer.de>
2025-05-25 10:17:02 +0200

    added splitx function and data structs for it

--- libft/inc/extra.h
+++ libft/inc/extra.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   extra.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: anakin <anakin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 17:19:22 by apregitz          #+#    #+#             */
-/*   Updated: 2025/03/13 13:44:32 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/05/25 09:54:12 by anakin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,37 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-char	*ft_strrev(char *string);
-char	*smash(const char **words, char seperator, size_t count);
-int		ft_swp(void *arr1, void *arr2, size_t size);
-ssize_t	ft_strclen(const char *str, char ch);
+typedef struct s_arg_data
+{
+	int		in_s_quote;
+	int		in_d_quote;
+	int		esc;
+	int		start;
+	int		pos;
+	char	*arg;
+	int		arg_len;
+	int		write_pos;
+	int		arg_count;
+	int		in_word;
+}			t_arg_data;
+
+typedef struct s_splitx_main_data
+{
+	char	**result;
+	int		arg_count;
+	int		current_arg;
+	int		pos;
+	int		next_pos;
+}			t_splitx_main_data;
+
+char		*ft_strrev(char *string);
+char		*smash(const char **words, char seperator, size_t count);
+int			ft_swp(void *arr1, void *arr2, size_t size);
+ssize_t		ft_strclen(const char *str, char ch);
+char		**ft_splitx(const char *s);
+void		ft_free_splitx(char **arr, int count);
+int			is_whitespacex(char c);
+int			skip_whitespacex(const char *s, int pos);
+int			calc_arg_lengthx(const char *s, int start);
 
 #endif
